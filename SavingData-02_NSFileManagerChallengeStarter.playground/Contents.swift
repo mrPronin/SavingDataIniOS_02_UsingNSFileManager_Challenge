1d83d158480aa1d5c6be522886107ba715f4aa5f
Alexander Pronin <pronin.alx@gmail.com>
2016-04-09 14:40:20 +0300

    Final commit

--- SavingData-02_NSFileManagerChallengeStarter.playground/Contents.swift
+++ SavingData-02_NSFileManagerChallengeStarter.playground/Contents.swift
@@ -11,13 +11,14 @@ import UIKit
 let files = ["animation", "intro", "particles", "unity2d", "vr"]
 //: Next, you'll have to get a reference to your file manager. To do this, call the static method "defaultManager" on the NSFileManager object.
     // Code goes here
-
+    let fileManager = NSFileManager.defaultManager()
 //: This next bit of code creates a variable to store a reference to the document directory.
 var documentDirectory: NSURL?
 
 do {
 //: Inside of the do-catch block, get a referece to the document directory by calling the URLForDirectory()
     // Code goes here
+    documentDirectory = try fileManager.URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: false)
 } catch {
   print(error)
 }
@@ -27,19 +28,28 @@ guard let documentDirectory = documentDirectory else {
   abort()
 }
 
-//: Now comes the fun part, first loop through each of the files.
-for file in files {
-//: First get a reference to the file from the bundle. You can call URLForResource on the NSBundle.mainBundle
-  // Code goes here
-//: Next, create a new url to the location of where the file will be saved.
-  // Code goes here
-//: Finally, copy the file url to the save location. This is copyItemAtURL().
-
-}
 
 do {
+    //: Now comes the fun part, first loop through each of the files.
+    for file in files {
+        //: First get a reference to the file from the bundle. You can call URLForResource on the NSBundle.mainBundle
+        // Code goes here
+        let currentURL = NSBundle.mainBundle().URLForResource(file, withExtension: "txt")
+        //: Next, create a new url to the location of where the file will be saved.
+        // Code goes here
+        let targetURL = documentDirectory.URLByAppendingPathComponent("\(file).txt")
+        //: Finally, copy the file url to the save location. This is copyItemAtURL().
+        try? fileManager.copyItemAtURL(currentURL!, toURL: targetURL)
+    }
 //: With your files saved, it's now time list them out. To do this, you're going to call to contentsOfDirectoryAtURL on the fileManager
     // Code goes here
+    let list = try fileManager.contentsOfDirectoryAtURL(documentDirectory, includingPropertiesForKeys: nil, options: .SkipsHiddenFiles)
+    for url in list {
+        //print(url)
+        let fileContentString = try? String(contentsOfURL: url)
+        print("\n" + url.lastPathComponent!)
+        print(fileContentString)
+    }
 //: Lastly, loop through the fileList and print them out.
     // Code goes here
 } catch {
